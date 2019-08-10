43c66bc27050c2c49f16815ef44c0839a0ccab48
67ng <2279142891@qq.com>
2019-08-10 09:46:52 +0800

    杨辉三角

---
+++ src/_118/Solution.java
@@ -0,0 +1,30 @@
+package _118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: 生成杨辉三角
+ * @Author: 67ng
+ * @Date: 2019/8/9 22:41
+ */
+public class Solution {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> allrows = new ArrayList<>();
+        ArrayList<Integer> row = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++)
+                row.set(j, row.get(j) + row.get(j + 1));
+            allrows.add(new ArrayList<>(row));
+        }
+        return allrows;
+
+
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+        System.out.println(generate(numRows));
+    }
+}
---
+++ src/_119/Solution.java
@@ -0,0 +1,29 @@
+package _119;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: 67ng
+ * @Date: 2019/8/9 22:42
+ */
+public class Solution {
+    public static List<Integer> getRow(int rowIndex) {
+        ArrayList<Integer> row = new ArrayList<>();
+        for (int i = 0; i < rowIndex + 1; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++)
+                row.set(j, row.get(j) + row.get(j + 1));
+
+        }
+        return row;
+
+    }
+
+
+    public static void main(String[] args) {
+        int rowIndx = 3;
+        System.out.println(getRow(rowIndx));
+    }
+}
