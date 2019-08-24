0c60043dfb3988f49450d1ebe86875891e4c9b31
67ng <2279142891@qq.com>
2019-08-24 21:56:31 +0800

    多阶斐波拉切数列和递归

--- src/_779/Solution.java
+++ src/_779/Solution.java
@@ -9,6 +9,22 @@ public class Solution {
 
     public int kthGrammar(int N, int K) {
 
+        if (N == 1 && K == 1) {
+            return 0;
+        } else {
+            int i = K % 2;
+            if (i == 0) {
+                K = K / 2;
+                N--;
+                return kthGrammar(N, K) == 1 ? 0 : 1;//偶数数字，上层对应数和当前数字不等
+            } else {
+                K = K / 2 + 1;
+                N--;
+                return kthGrammar(N, K) == 1 ? 1 : 0;//奇数数字，上层对应数和当前数字相等
+            }
+        }
+
+
     }
 
 }
