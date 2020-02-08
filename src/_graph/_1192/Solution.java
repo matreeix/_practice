45b7307b8ec571f0cbb008b13242ddf72f84ed8a
67ng <2279142891@qq.com>
2020-02-08 23:38:55 +0800

    哈米尔顿路径的应用

---
+++ src/_graph/_1192/Solution.java
@@ -0,0 +1,25 @@
+package _graph._1192;
+
+/**
+ * @Description:查找集群内的关键连接点（寻找桥）
+ *
+ * 力扣数据中心有 n 台服务器，分别按从 0 到 n-1 的方式进行了编号。
+ *
+ * 它们之间以「服务器到服务器」点对点的形式相互连接组成了一个内部集群，其中连接 connections 是无向的。
+ *
+ * 从形式上讲，connections[i] = [a, b] 表示服务器 a 和 b 之间形成连接。任何服务器都可以直接或者间接地通过网络到达任何其他服务器。
+ *
+ * 「关键连接」是在该集群中的重要连接，也就是说，假如我们将它移除，便会导致某些服务器无法访问其他服务器。
+ *
+ * 请你以任意顺序返回该集群内的所有 「关键连接」
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/critical-connections-in-a-network
+ *
+ * @Author: 67ng
+ * @Date: 2020/2/8
+ */
+public class Solution {
+
+
+}
