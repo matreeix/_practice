abc3a382d34f39d41e3694962223f5c5991d932b
mzh <ma.zhenhua@datasw.com>
2020-02-18 15:56:03 +0800

    整型的溢出

---
+++ src/_math/_367/Solution.java
@@ -0,0 +1,25 @@
+package _math._367;
+
+/**
+ * @description ：判断平方数
+ * @date ： 2020-02-18
+ */
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        return search(1, num, num);
+    }
+    public boolean search(int left, int right, int num) {
+        if (left > right)
+            return false;
+        int mid = (right - left) / 2 + left;
+
+        if (num > mid * mid  && mid < 46341) {//46340是不大于最大整型的最大平方根数
+            return search(mid + 1, right, num);
+        } else if (mid >= 46341 || num < mid * mid) {
+            return search(left, mid - 1, num);
+        } else {
+            return true;
+        }
+    }
+
+}
