cbe4b5ca7010bb9c94e135e24f3c98cfd21dfb6b
67ng <2279142891@qq.com>
2020-03-17 23:54:00 +0800

    一维和二维数组求指定索引区间和

--- src/_stack/_739/Solution.java
+++ src/_stack/_739/Solution.java
@@ -30,6 +30,7 @@ public class Solution {
         int[] stack = new int[T.length];
         int top = -1;
         int[] ret = new int[T.length];
+
         for (int i = 0; i < T.length; i++) {
             while (top > -1 && T[i] > T[stack[top]]) {
                 int idx = stack[top--];
