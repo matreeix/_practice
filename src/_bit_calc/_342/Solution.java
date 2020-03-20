849767c086ccba2ae6ad0d599ed2fa786d42ce35
67ng <2279142891@qq.com>
2020-03-20 23:53:44 +0800

    位运算的应用求4的幂次

---
+++ src/_bit_calc/_342/Solution.java
@@ -0,0 +1,15 @@
+package _bit_calc._342;
+
+/**
+ * @Description: 判断一个数是否是4的幂次方
+ * @Author: 67ng
+ * @Date: 2020/3/20
+ */
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0
+                && (num&(num-1)) == 0 //确保为2的幂次
+                && (num & 0x55555555) != 0;//确保1的位置在奇数位置
+    }
+
+}
