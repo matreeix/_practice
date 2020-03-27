f5a67a0ad9568f7711f306b44166e5501a31eb8e
67ng <2279142891@qq.com>
2020-03-27 21:47:37 +0800

    二叉树的遍历的应用3

--- src/_tree/_binary_tree/_230/Solution.java
+++ src/_tree/_binary_tree/_230/Solution.java
@@ -17,16 +17,17 @@ public class Solution {
     }
 
     public int kthSmallest(TreeNode root, int k) {
-        int left = nodeCount(root.left);  // this value can be saved in the root node
-        if (left + 1 == k) {
+        int left = nodeCount(root.left); //统计根节点左子树节点数
+        if (left + 1 == k) {//第k小刚好是根节点
             return root.val;
-        } else if (left + 1 < k) {
+        } else if (left + 1 < k) {//在右子树时
             return kthSmallest(root.right, k - left - 1);
-        } else {
+        } else {//在左子树时
             return kthSmallest(root.left, k);
         }
     }
 
+    //统计节点个数
     private int nodeCount(TreeNode root) {
         if (root == null)
             return 0;
