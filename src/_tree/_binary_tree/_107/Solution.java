7ec364a3f6791d9417fa63ddda0c2228dc0b191e
67ng <2279142891@qq.com>
2020-03-31 23:13:49 +0800

    修饰

---
+++ src/_tree/_binary_tree/_107/Solution.java
@@ -0,0 +1,45 @@
+package _tree._binary_tree._107;
+
+import java.util.*;
+
+/**
+ * @Description:
+ * @Author: 67ng
+ * @Date: 2020/3/30
+ */
+public class Solution {
+
+    //Definition for a binary tree node.
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+        while ( !queue.isEmpty() ) {
+            levels.add(new ArrayList<Integer>());
+            int level_length = queue.size();
+            for(int i = 0; i < level_length; ++i) {
+                TreeNode node = queue.remove();
+                levels.get(level).add(node.val);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            level++;
+        }
+        Collections.reverse(levels);
+        return levels;
+    }
+}
