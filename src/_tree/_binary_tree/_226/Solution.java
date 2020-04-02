8358ea2737d0fb14d8dd9844b9c1beab476ddde0
67ng <2279142891@qq.com>
2020-04-02 23:28:42 +0800

    翻转二叉树

---
+++ src/_tree/_binary_tree/_226/Solution.java
@@ -0,0 +1,29 @@
+package _tree._binary_tree._226;
+
+/**
+ * @Description: 翻转二叉树
+ * @Author: 67ng
+ * @Date: 2020/4/2
+ */
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode temp = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(temp);
+
+        return root;
+    }
+
+}
