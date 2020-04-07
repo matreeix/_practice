23afa73dc1a59fd02c27644e7f44d17d2f6c9cc1
mzh <ma.zhenhua@datasw.com>
2020-04-07 17:12:41 +0800

    二叉树转字符串

---
+++ src/_tree/_binary_tree/_606/Solution.java
@@ -0,0 +1,24 @@
+package _tree._binary_tree._606;
+
+/**
+ * @description ：二叉树转字符串
+ * @date ： 2020-04-07
+ */
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public String tree2str(TreeNode t) {
+        if (t == null) return "";
+        if (t.left == null && t.right == null) return t.val + "";
+        if (t.right == null)
+            return t.val + "(" + tree2str(t.left) + ")";
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
+}
