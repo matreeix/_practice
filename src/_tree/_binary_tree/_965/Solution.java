3a6216c28e0e3f9ae97e43c5ece90ef423c0986b
67ng <2279142891@qq.com>
2020-04-09 21:30:36 +0800

    单值二叉树

---
+++ src/_tree/_binary_tree/_965/Solution.java
@@ -0,0 +1,33 @@
+package _tree._binary_tree._965;
+
+/**
+ * @Description: 单值二叉树
+ * @Author: 67ng
+ * @Date: 2020/4/4
+ */
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        boolean left_correct = (root.left == null ||
+                (root.val == root.left.val && isUnivalTree(root.left)));
+        boolean right_correct = (root.right == null ||
+                (root.val == root.right.val && isUnivalTree(root.right)));
+        return left_correct && right_correct;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+        System.out.println((new Solution()).isUnivalTree(root));
+    }
+}
