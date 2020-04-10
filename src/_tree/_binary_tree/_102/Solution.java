d853d58cd9c83e21e8f1581516f0086552f0133f
mzh <ma.zhenhua@datasw.com>
2020-04-10 14:42:06 +0800

    二叉树的层序遍历3

---
+++ src/_tree/_binary_tree/_102/Solution.java
@@ -0,0 +1,51 @@
+package _tree._binary_tree._102;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @description ：
+ * @date ： 2020-04-10
+ */
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null)
+            return res;
+
+        //使用LinkedList来做为我们的先入先出的队列
+        LinkedList<Pair<TreeNode, Integer>> queue = new LinkedList<Pair<TreeNode, Integer>>();
+        queue.addLast(new Pair<TreeNode, Integer>(root, 0));
+
+        while(!queue.isEmpty()){
+
+            Pair<TreeNode, Integer> front = queue.removeFirst();
+            TreeNode node = front.getKey();
+            int level = front.getValue();
+
+            if(level == res.size())
+                res.add(new ArrayList<Integer>());
+            assert level < res.size();
+
+            res.get(level).add(node.val);
+            if(node.left != null)
+                queue.addLast(new Pair<>(node.left, level + 1));
+            if(node.right != null)
+                queue.addLast(new Pair<>(node.right, level + 1));
+        }
+
+        return res;
+    }
+}
