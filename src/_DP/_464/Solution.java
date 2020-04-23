62922ec3f2733628e8e513fab920dc13db706bf4
67ng <2279142891@qq.com>
2020-04-23 23:54:09 +0800

    最大正方形

---
+++ src/_DP/_464/Solution.java
@@ -0,0 +1,21 @@
+package _DP._464;
+
+/**
+ * @Description:
+ * @Author: 67ng
+ * @Date: 2020/4/19
+ */
+public class Solution {
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (maxChoosableInteger >= desiredTotal) return true;
+        int sum = maxChoosableInteger + 1;
+        int res = desiredTotal / sum;
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
