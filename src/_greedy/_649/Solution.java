46a935e7a8ddef8498d9249dded3818be4242605
Pythagodzilla <2279142891@qq.com>
2020-06-02 22:20:23 +0800

    在二叉树中增加一行

--- src/_greedy/_649/Solution.java
+++ src/_greedy/_649/Solution.java
@@ -10,6 +10,7 @@ import java.util.*;
  * @Date: 2020/6/1
  */
 public class Solution {
+    //使用队列
     public static String predictPartyVictory(String senate) {
         Queue<Integer> queue = new LinkedList();
         int[] people = new int[]{0, 0};//记录R和D的数量
@@ -38,9 +39,9 @@ public class Solution {
     public String predictPartyVictory2(String senate) {
         boolean R = true, D = true;
         char[] arr = senate.toCharArray();
-        //标记变量，当person > 0的时候，此时R方有权利把D方的成员淘汰掉
-        //当person < 0的时候，D方有权利把R方的成员淘汰掉
-        int person = 0;
+        //标记变量，ban > 0的时候，此时R方有权利把D方的成员淘汰掉
+        //ban < 0的时候，D方有权利把R方的成员淘汰掉
+        int ban = 0;//记录ban权重，妙啊！！！
         while (R && D) {
             //当遇到双方成员的时候置为true，表示当前还有人员存在
             //当R或D为false，表示一方已经没有成员存在了，胜负已分，结束循环
@@ -49,18 +50,18 @@ public class Solution {
             for (int i = 0; i < arr.length; ++i) {
                 if (arr[i] == 'R') {
                     R = true;
-                    if (person < 0)
+                    if (ban < 0)
                         arr[i] = 'O';
-                    ++person;
+                    ++ban;
                 } else if (arr[i] == 'D') {
                     D = true;
-                    if (person > 0)
+                    if (ban > 0)
                         arr[i] = 'O';
-                    --person;
+                    --ban;
                 }
             }
         }
-        return person > 0 ? "Radiant" : "Dire";
+        return ban > 0 ? "Radiant" : "Dire";
     }
 
     public static void main(String[] args) {
---
+++ src/_tree/_binary_tree/_623/Solution.java
@@ -0,0 +1,100 @@
+package _tree._binary_tree._623;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Description: 在二叉树中增加一行
+ * @Author: 67ng
+ * @Date: 2020/6/1
+ */
+public class Solution {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode addOneRow(TreeNode t, int v, int d) {
+        if (d == 1) {
+            TreeNode n = new TreeNode(v);
+            n.left = t;
+            return n;
+        }
+        insert(t, v, 1, d);
+        return t;
+    }
+
+    //前序深度优先遍历插入
+    public void insert(TreeNode node, int val, int depth, int n) {
+        if (node == null)
+            return;
+        if (depth == n - 1) {
+            TreeNode t = node.left;
+            node.left = new TreeNode(val);
+            node.left.left = t;
+            t = node.right;
+            node.right = new TreeNode(val);
+            node.right.right = t;
+        } else {
+            insert(node.left, val, depth + 1, n);
+            insert(node.right, val, depth + 1, n);
+        }
+    }
+
+    //广度优先遍历
+    public TreeNode addOneRow2(TreeNode t, int v, int d) {
+        if (d == 1) {
+            TreeNode n = new TreeNode(v);
+            n.left = t;
+            return n;
+        }
+        Queue< TreeNode > queue = new LinkedList< >();
+        queue.add(t);
+        int depth = 1;
+        while (depth < d - 1) {
+            Queue < TreeNode > temp = new LinkedList < > ();
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.remove();
+                if (node.left != null) temp.add(node.left);
+                if (node.right != null) temp.add(node.right);
+            }
+            queue = temp;
+            depth++;
+        }
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            TreeNode temp = node.left;
+            node.left = new TreeNode(v);
+            node.left.left = temp;
+            temp = node.right;
+            node.right = new TreeNode(v);
+            node.right.right = temp;
+        }
+        return t;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(1);
+        root.right.left = new TreeNode(5);
+        TreeNode res = (new Solution()).addOneRow(root, 1, 2);
+        System.out.println(res.val);
+        System.out.print(res.left.val + " ");
+        System.out.println(res.right.val);
+        System.out.print(res.left.left.val + " ");
+        System.out.println(res.right.right.val);
+        System.out.print(res.left.left.left.val + " ");
+        System.out.print(res.left.left.right.val + " ");
+        System.out.println(res.right.right.left.val);
+
+    }
+
+}
