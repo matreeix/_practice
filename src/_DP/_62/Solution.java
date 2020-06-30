78cb224bacf45af402a8c1d6ed9fb4690ea8a837
Pythagodzilla <2279142891@qq.com>
2020-06-30 23:40:38 +0800

    不同路径的数学方法

--- src/_DP/_62/Solution.java
+++ src/_DP/_62/Solution.java
@@ -11,6 +11,7 @@ package _DP._62;
  */
 
 public class Solution {
+    //dp
     public int uniquePaths(int m, int n) {
         int[][] mem = new int[m][n];
         for (int i = 0; i < m; i++) {
@@ -22,6 +23,26 @@ public class Solution {
             }
         }
         return mem[m - 1][n - 1];
+    }
+
+    //数学法，总排列=（m + n）！/（m！* n！）
+    public int uniquePaths2(int m, int n) {
+        if (m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if (m < n) {// Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long res = 1;
+        int j = 1;
+        for (int i = m + 1; i <= m + n; i++, j++) {//Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+        return (int) res;
     }
 
     public static void main(String[] args) {
