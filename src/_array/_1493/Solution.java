c37db3e30877c4f2981160e75d859e0bb06347fd
Pythagodzilla <2279142891@qq.com>
2020-07-06 19:35:18 +0800

    1491~1493

---
+++ src/_array/_1493/Solution.java
@@ -0,0 +1,54 @@
+package _array._1493;
+
+import java.util.Arrays;
+
+/**
+ * @Description: 删除一个元素后只含有1的最长子数组
+ * <p>
+ * 给你一个二进制数组 nums ，你需要从中删掉一个元素。
+ * 请你在删掉元素的结果数组中，返回最长的且只包含 1 的非空子数组的长度。
+ * 如果不存在这样的子数组，请返回 0 。
+ * @Author: Pythagodzilla
+ * @Date: 2020/7/6
+ */
+
+public class Solution {
+    //滑动窗口，窗口伸缩
+    public int longestSubarray(int[] A) {
+        int i = 0, j, k = 1, res = 0;
+        for (j = 0; j < A.length; ++j) {
+            if (A[j] == 0) k--;//移动右窗口
+            while (k < 0) {
+                if (A[i] == 0) k++;
+                i++;//移动左窗口
+            }
+            res = Math.max(res, j - i);
+        }
+        return res;
+    }
+
+    //窗口不会缩小
+    //For each A[j], try to find the longest subarray.
+    //If A[i] ~ A[j] has zeros <= 1, we continue to increment j.
+    //If A[i] ~ A[j] has zeros > 1, we increment i.
+    public int longestSubarray2(int[] A) {
+        int i = 0, j, k = 1;
+        for (j = 0; j < A.length; ++j) {
+            if (A[j] == 0) k--;
+            if (k < 0 && A[i++] == 0) k++;//妙啊！
+        }
+        return j - i - 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 1, 0, 1};
+        int[] nums2 = {0, 1, 1, 1, 0, 1, 1, 0, 1};
+        int[] nums3 = {1, 1, 1};
+        int[] nums4 = {0, 0, 0};
+
+        System.out.println((new Solution()).longestSubarray2(nums1));//3
+        System.out.println((new Solution()).longestSubarray2(nums2));//5
+        System.out.println((new Solution()).longestSubarray2(nums3));//2
+        System.out.println((new Solution()).longestSubarray2(nums4));//0
+    }
+}
---
+++ src/_math/_1492/Solution.java
@@ -0,0 +1,83 @@
+package _math._1492;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: n的第k个因子
+ * <p>
+ * 给你两个正整数 n 和 k 。
+ * 如果正整数 i 满足 n % i == 0 ，那么我们就说正整数 i 是整数 n 的因子。
+ * 考虑整数 n 的所有因子，将它们 升序排列 。请你返回第 k 个因子。如果 n 的因子数少于 k ，请你返回 -1 。
+ * @Author: Pythagodzilla
+ * @Date: 2020/7/6
+ */
+
+public class Solution {
+    public int kthFactor(int n, int k) {
+        int sqrt = (int) Math.sqrt(n);
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 1; i <= sqrt; i++) {
+            if (n % i == 0)
+                factors.add(i);
+        }
+
+        if (k <= factors.size()) {
+            return factors.get(k - 1);
+        } else if (k <= 2 * factors.size() - 1) {
+            return n / (factors.get(2 * factors.size() - k));
+        } else {
+            if (sqrt * sqrt != n && k == 2 * factors.size())
+                return n;
+            return -1;
+        }
+    }
+
+    public int kthFactor2(int n, int k) {
+        int d = 1;
+        for (; d * d <= n; ++d)
+            if (n % d == 0 && --k == 0)
+                return d;
+        for (d = d - 1; d >= 1; --d) {
+            if (d * d == n)
+                continue;
+            if (n % d == 0 && --k == 0)
+                return n / d;
+        }
+        return -1;
+    }
+
+    public int kthFactor3(int n, int k) {
+
+        if (n == 1 || k == 1)
+            return 1;
+        if (k > n / 2 + 1)
+            return -1;
+
+        int mid = 0;
+        // 60 : 30 20 15
+        if (n % 2 == 0)
+            // 偶数：1 2
+            mid = n / 2;
+        else
+            // 基数
+            mid = n / 2 + 1;
+
+        int count = 2;
+        for (int i = 2; i <= mid; i++)
+            if (n % i == 0 && k == count++)
+                return i;
+
+        if (count == k)
+            return n;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new Solution()).kthFactor(12, 3));
+        System.out.println((new Solution()).kthFactor(1000, 5));
+        System.out.println((new Solution()).kthFactor(1000, 11));
+        System.out.println((new Solution()).kthFactor(7, 2));
+        System.out.println((new Solution()).kthFactor(4, 4));
+    }
+}
