b23dbd48a82d15d8b9ac48613824ba6a34321efc
Pythagodzilla <2279142891@qq.com>
2020-07-24 17:50:49 +0800

    N皇后问题II

--- src/backtracking/_52/Solution.java
+++ src/backtracking/_52/Solution.java
@@ -1,7 +1,10 @@
 package backtracking._52;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * @Description: N皇后问题
+ * @Description: N皇后问题II
  * @Author: 67ng
  * @Date: 2019/8/26 21:44
  */
@@ -53,8 +56,46 @@ public class Solution {
         return true;
     }
 
+    private final Set<Integer> occupiedCols = new HashSet<Integer>();
+    private final Set<Integer> occupiedDiag1s = new HashSet<Integer>();
+    private final Set<Integer> occupiedDiag2s = new HashSet<Integer>();
+
+    public int totalNQueens2(int n) {
+        return totalNQueensHelper(0, 0, n);
+    }
+
+    private int totalNQueensHelper(int row, int count, int n) {
+        for (int col = 0; col < n; col++) {
+            if (occupiedCols.contains(col))
+                continue;
+            int diag1 = row - col;
+            if (occupiedDiag1s.contains(diag1))
+                continue;
+            int diag2 = row + col;
+            if (occupiedDiag2s.contains(diag2))
+                continue;
+            // we can now place a queen here
+            if (row == n - 1)
+                count++;
+            else {
+                occupiedCols.add(col);
+                occupiedDiag1s.add(diag1);
+                occupiedDiag2s.add(diag2);
+                count = totalNQueensHelper(row + 1, count, n);
+                // recover
+                occupiedCols.remove(col);
+                occupiedDiag1s.remove(diag1);
+                occupiedDiag2s.remove(diag2);
+            }
+        }
+
+        return count;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(totalNQueens(1));
+        System.out.println(totalNQueens(4));
         System.out.println(totalNQueens(8));
         System.out.println(totalNQueens(9));
     }
