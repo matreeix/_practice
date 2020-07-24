c68adced74f022feb3c47b45cffce887f5d593a5
Pythagodzilla <2279142891@qq.com>
2020-07-24 18:22:19 +0800

    杂题

---
+++ src/_string/_58/Solution.java
@@ -0,0 +1,18 @@
+package _string._58;
+
+/**
+ * @Description: 58. 最后一个单词的长度
+ *
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+ * 如果不存在最后一个单词，请返回 0 。
+ * 说明：一个单词是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ *
+ * @Author: Pythagodzilla
+ * @Date: 2020/7/24
+ */
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        return s.trim().length()-s.trim().lastIndexOf(" ")-1;
+    }
+}
