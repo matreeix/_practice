2193f9e412ecfa4ffb2f28caffe42eae74200c0f
Pythagodzilla <2279142891@qq.com>
2020-07-27 00:24:31 +0800

    周赛199第二题，灯泡开关IV

---
+++ src/_CONTEST/_weekly/_199/Solution2.java
@@ -0,0 +1,40 @@
+package _CONTEST._weekly._199;
+
+/**
+ * @Description: 5473. 灯泡开关 IV
+ * <p>
+ * 房间中有 n 个灯泡，编号从 0 到 n-1 ，自左向右排成一行。最开始的时候，所有的灯泡都是 关 着的。
+ * 请你设法使得灯泡的开关状态和 target 描述的状态一致，其中 target[i] 等于 1 第 i 个灯泡是开着的，等于 0 意味着第 i 个灯是关着的。
+ * 有一个开关可以用于翻转灯泡的状态，翻转操作定义如下：
+ * <p>
+ * 1.选择当前配置下的任意一个灯泡（下标为 i ）
+ * 2.翻转下标从 i 到 n-1 的每个灯泡
+ * <p>
+ * 翻转时，如果灯泡的状态为 0 就变为 1，为 1 就变为 0 。
+ * 返回达成 target 描述的状态所需的 最少 翻转次数
+ * @Author: Pythagodzilla
+ * @Date: 2020/7/26
+ */
+
+public class Solution2 {
+    public static int minFlips(String target) {
+        char mark = '0';
+        int res = 0;
+        for (char ch : target.toCharArray()) {
+            if (ch == mark) {//当前字符和标记相同，继续
+                continue;
+            } else {//不同时进行翻转
+                res++;
+                mark = mark == '0' ? '1' : '0';
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minFlips("10111"));//3
+        System.out.println(minFlips("101"));//3
+        System.out.println(minFlips("001011101"));//5
+        System.out.println(minFlips("0000"));//0
+    }
+}
