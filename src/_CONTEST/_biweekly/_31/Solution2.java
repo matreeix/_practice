8217ce05b3b7f640ef9c8ef15bc9e5077e6e5b01
Pythagodzilla <2279142891@qq.com>
2020-07-28 16:49:02 +0800

    2、4题

---
+++ src/_CONTEST/_biweekly/_31/Solution2.java
@@ -0,0 +1,76 @@
+package _CONTEST._biweekly._31;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description: 1524. 和为奇数的子数组数目
+ * 给你一个整数数组 arr 。请你返回和为 奇数 的子数组数目。
+ * 由于答案可能会很大，请你将结果对 10^9 + 7 取余后返回。
+ * @Author: Pythagodzilla
+ * @Date: 2020/7/27
+ */
+
+public class Solution2 {
+
+    //LTE O(n^2)，子数组的奇数个数为奇数时，其和才为奇数
+    public static int numOfSubarrays(int[] arr) {
+        int n = arr.length;
+        int[] odd = new int[n];//[0,i]的奇数个数
+        odd[0] = arr[0] % 2 == 0 ? 0 : 1;
+//        int[] sub_odd = new int[n * (n + 1) / 2];//数组太大,存不了
+        int[] sub_odd = new int[n + 1];//储存频数，最多有n个奇数
+        sub_odd[odd[0]]++;
+        for (int i = 1; i < odd.length; i++) {
+            if (arr[i] % 2 != 0) odd[i] = odd[i - 1] + 1;
+            else odd[i] = odd[i - 1];
+
+            sub_odd[odd[i]]++;
+            for (int j = 0; j <= i - 1; j++)
+                sub_odd[odd[i] - odd[j]]++;
+        }
+
+        int res = 0;
+        for (int i = 0; i < sub_odd.length; i++)
+            if (i % 2 != 0)
+                res += sub_odd[i];
+        return res;
+    }
+
+    //O(n)
+    public int numOfSubarrays2(int[] arr) {
+        int sum = 0;
+        for (int i = 0, odd = 0; i < arr.length; ++i) {
+            if (arr[i] % 2 == 1)
+                odd = i - odd + 1;
+            sum = (sum + odd) % 1000000007;
+        }
+        return sum;
+    }
+
+    //前缀和,O(n)
+    public int numOfSubarrays3(int[] A) {
+        int cur = 0, res = 0, count[] = {1, 0}, mod = (int)1e9 + 7;
+        for (int a: A) {
+            cur ^= a & 1;
+            res = (res + count[1 - cur]) % mod;
+            count[cur]++;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+        int[] arr3 = {1, 2, 3, 4, 5, 6, 7};
+        int[] arr4 = {100, 100, 99, 99};
+        int[] arr5 = {7};
+        System.out.println(numOfSubarrays(arr1));//4
+        System.out.println(numOfSubarrays(arr2));//0
+        System.out.println(numOfSubarrays(arr3));//16
+        System.out.println(numOfSubarrays(arr4));//4
+        System.out.println(numOfSubarrays(arr5));//1
+    }
+}
--- src/_CONTEST/_biweekly/_31/Solution3.java
+++ src/_CONTEST/_biweekly/_31/Solution3.java
@@ -34,6 +34,19 @@ public class Solution3 {
         return end - start;
     }
 
+    public int numSplits2(String str) {
+        int l[] = new int[26], r[] = new int[26], d_l = 0, d_r = 0, res = 0;
+        char[] s = str.toCharArray();
+        for (char ch : s)
+            d_r += ++r[ch - 'a'] == 1 ? 1 : 0;
+        for (int i = 0; i < s.length; ++i) {
+            d_l += ++l[s[i] - 'a'] == 1 ? 1 : 0;
+            d_r -= --r[s[i] - 'a'] == 0 ? 1 : 0;
+            res += d_l == d_r ? 1 : 0;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println(numSplits("aacaba"));//2
         System.out.println(numSplits("abcd"));//1
