74353cb3a1f9e5b084585afa36177b857bd53386
Pythagodzilla <2279142891@qq.com>
2020-08-01 16:25:10 +0800

    链表系列题

---
+++ src/_Cracking_the_Coding_Interview/_02/_02/Solution.java
@@ -0,0 +1,34 @@
+package _Cracking_the_Coding_Interview._02._02;
+
+/**
+ * @Description: 面试题 02.02. 返回倒数第 k 个节点
+ * 实现一种算法，找出单向链表中倒数第 k 个节点。返回该节点的值。
+ * @Author: Pythagodzilla
+ * @Date: 2020/7/31
+ */
+
+public class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public int kthToLast(ListNode head, int k) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (k>0){
+            fast =fast.next;
+            k-=1;
+        }
+
+        while (fast!=null){
+            fast =fast.next;
+            slow = slow.next;
+        }
+        return slow.val;
+    }
+}
---
+++ src/_Cracking_the_Coding_Interview/_02/_04/Solution.java
@@ -0,0 +1,44 @@
+package _Cracking_the_Coding_Interview._02._04;
+
+/**
+ * @Description: 面试题 02.04. 分割链表
+ * 编写程序以 x 为基准分割链表，使得所有小于 x 的节点排在大于或等于 x 的节点之前。
+ * 如果链表中包含 x，x 只需出现在小于 x 的元素之后(如下所示)。分割元素 x 只需处于“右半部分”即可，其不需要被置于左右两部分之间。
+ * @Author: Pythagodzilla
+ * @Date: 2020/7/31
+ */
+
+public class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode minHead = new ListNode(Integer.MIN_VALUE);
+        ListNode min = minHead;
+        ListNode maxHead = new ListNode(Integer.MAX_VALUE);
+        ListNode max = maxHead;
+        while (head != null) {
+            if (head.val < x) {
+                min.next = new ListNode(head.val);
+                min = min.next;
+            } else {
+                max.next = new ListNode(head.val);
+                max = max.next;
+            }
+            head = head.next;
+        }
+        min.next = maxHead.next;
+        return minHead.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
