2bd3aaff3b745afe6d757d28af9bb29b97e95fa6
matreeix <2279142891@qq.com>
2020-08-24 14:09:31 +0800

    周赛203

---
+++ src/_CONTEST/_weekly/_203/Solution1.java
@@ -0,0 +1,31 @@
+package _CONTEST._weekly._203;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: 5495. 圆形赛道上经过次数最多的扇区
+ * <p>
+ * 给你一个整数 n 和一个整数数组 rounds 。有一条圆形赛道由 n 个扇区组成，扇区编号从 1 到 n 。
+ * 现将在这条赛道上举办一场马拉松比赛，该马拉松全程由 m 个阶段组成。其中，第 i 个阶段将会从扇区 rounds[i - 1] 开始，
+ * 到扇区 rounds[i] 结束。举例来说，第 1 阶段从 rounds[0] 开始，到 rounds[1] 结束。
+ * 请你以数组形式返回经过次数最多的那几个扇区，按扇区编号 升序 排列。
+ * 注意，赛道按扇区编号升序逆时针形成一个圆（请参见第一个示例）。
+ * @Author: matreeix
+ * @Date: 2020/8/23
+ */
+
+public class Solution1 {
+
+    public List<Integer> mostVisited(int n, int[] A) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = A[0]; i <= A[A.length - 1]; ++i)
+            res.add(i);
+        if (res.size() > 0) return res;
+        for (int i = 1; i <= A[A.length - 1]; ++i)
+            res.add(i);
+        for (int i = A[0]; i <= n; ++i)
+            res.add(i);
+        return res;
+    }
+}
---
+++ src/_CONTEST/_weekly/_203/Solution2.java
@@ -0,0 +1,38 @@
+package _CONTEST._weekly._203;
+
+import java.util.Arrays;
+
+/**
+ * @Description: 5496. 你可以获得的最大硬币数目
+ * 有 3n 堆数目不一的硬币，你和你的朋友们打算按以下方式分硬币：
+ * 每一轮中，你将会选出 任意 3 堆硬币（不一定连续）。
+ * Alice 将会取走硬币数量最多的那一堆。
+ * 你将会取走硬币数量第二多的那一堆。
+ * Bob 将会取走最后一堆。
+ * 重复这个过程，直到没有更多硬币。
+ * 给你一个整数数组 piles ，其中 piles[i] 是第 i 堆中硬币的数目。
+ * <p>
+ * 返回你可以获得的最大硬币数目。
+ * @Author: matreeix
+ * @Date: 2020/8/23
+ */
+
+public class Solution2 {
+
+    public static int maxCoins(int[] piles) {
+        int cnt = piles.length / 3;
+        Arrays.sort(piles);
+        int res = 0;
+        for (int i = piles.length - 2; cnt > 0; i -= 2) {
+            res += piles[i];
+            cnt--;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {9, 8, 7, 6, 5, 1, 2, 3, 4};
+        System.out.println(maxCoins(arr));
+    }
+}
