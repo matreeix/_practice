6e087df25e123baa31fa402853c707970d73d715
matreeix <2279142891@qq.com>
2020-08-28 00:10:02 +0800

    添加main方法

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @Description: Leetcode刷题总结
+ * @Author: matreeix
+ * @Date: 2020/8/26
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello,LeetCode!");
+    }
+}
+
