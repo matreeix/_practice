fd48300708b2b41af95a40f7617272461d8d172f
matreeix <2279142891@qq.com>
2021-01-01 16:05:38 +0800

    方法二

--- src/_Cracking_the_Coding_Interview/_17/_18/Solution.java
+++ src/_Cracking_the_Coding_Interview/_17/_18/Solution.java
@@ -20,15 +20,15 @@ public class Solution {
         for (int s : small)
             smallSet.add(s);
         int len = Integer.MAX_VALUE;
-        int[] ans={-1,-1};
+        int[] ans = {-1, -1};
         for (int i = 0; i < b_len; ++i) {
             if (smallSet.contains(big[i])) {
                 list.add(i);
                 bigSet.add(big[i]);
-                if(map.containsKey(big[i]))
-                    map.put(big[i],map.get(big[i])+1);
+                if (map.containsKey(big[i]))
+                    map.put(big[i], map.get(big[i]) + 1);
                 else
-                    map.put(big[i],1);
+                    map.put(big[i], 1);
 
                 if (bigSet.size() == smallSet.size()) {
                     int temp;
@@ -49,8 +49,60 @@ public class Solution {
                 }
             }
         }
-        if(ans[0]<0)
+        if (ans[0] < 0)
             return new int[0];
         return ans;
+    }
+
+    public int[] shortestSeq2(int[] big, int[] small) {
+        int n = big.length, m = small.length;
+        if (n < m) {
+            return new int[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>(n);
+        for (int x : small) {
+            map.put(x, 1);
+        }
+        // 双指针左端点
+        int l = 0;
+        // 已经找出多少个数
+        int cnt = 0;
+        // 记录答案
+        int ans = n + 1, x = -1, y = -1;
+        // 枚举双指针右端点
+        for (int r = 0; r < n; r++) {
+            Integer num = map.get(big[r]);
+            if (num == null) {
+                continue;
+            }
+            num--;
+            map.put(big[r], num);
+            if (num == 0) {
+                cnt++;
+            }
+            if (cnt == m) {
+                while (l < r) {
+                    num = map.get(big[l]);
+                    if (num != null) {
+                        num++;
+                        if (num > 0) {
+                            break;
+                        }
+                        map.put(big[l], num);
+                    }
+                    l++;
+                }
+                if (ans > r - l + 1) {
+                    ans = r - l + 1;
+                    x = l;
+                    y = r;
+                }
+            }
+        }
+        if (ans == n + 1) {
+            return new int[0];
+        }
+        return new int[]{x, y};
+
     }
 }
