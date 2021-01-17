6233b8aa9990a42faa862901fc858415cf8299d5
matreeix <2279142891@qq.com>
2021-01-17 17:41:35 +0800

    周赛224 1、2题

---
+++ src/_CONTEST/_weekly/_224/Solution1.java
@@ -0,0 +1,26 @@
+package _CONTEST._weekly._224;
+
+/**
+ * @Description: 5653. 可以形成最大正方形的矩形数目
+ * 给你一个数组 rectangles ，其中 rectangles[i] = [li, wi] 表示第 i 个矩形的长度为 li 、宽度为 wi 。
+ * 如果存在 k 同时满足 k <= li 和 k <= wi ，就可以将第 i 个矩形切成边长为 k 的正方形。例如，矩形 [4,6] 可以切成边长最大为 4 的正方形。
+ * 设 maxLen 为可以从矩形数组 rectangles 切分得到的 最大正方形 的边长。
+ * 返回可以切出边长为 maxLen 的正方形的矩形 数目 。
+ * @Date: 2021/1/17
+ */
+
+public class Solution1 {
+    public int countGoodRectangles(int[][] rectangles) {
+        int maxLen = 0, cnt = 0;
+        for (int[] rectangle : rectangles) {
+            int min = Math.min(rectangle[0], rectangle[1]);
+            if (min > maxLen) {
+                maxLen = min;
+                cnt = 1;
+            } else if (min == maxLen) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
---
+++ src/_CONTEST/_weekly/_224/Solution2.java
@@ -0,0 +1,33 @@
+package _CONTEST._weekly._224;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: 1726. 同积元组
+ * 给你一个由 不同 正整数组成的数组 nums ，请你返回满足 a * b = c * d 的元组 (a, b, c, d) 的数量。其中 a、b、c 和 d 都是 nums 中的元素，且 a != b != c != d 。
+ * 提示：
+ * 1 <= nums.length <= 1000
+ * 1 <= nums[i] <= 10^4
+ * nums 中的所有元素 互不相同
+ * @Date: 2021/1/17
+ */
+
+public class Solution2 {
+    public int tupleSameProduct(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();//<乘积，个数>
+        for (int i = 0; i < nums.length; i++)
+            for (int j = i + 1; j < nums.length; j++)
+                map.put(nums[i] * nums[j], map.getOrDefault(nums[i] * nums[j], 0) + 1);
+        int res = 0;
+        for (int i : map.values())
+            if (i > 1)
+                res += i * (i - 1) / 2;
+
+        return 8 * res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
