dd2b77ebb5f70f32cb620a39e260b49a914c491c
matreeix <2279142891@qq.com>
2021-02-20 23:50:11 +0800

    1754. 构造字典序最大的合并字符串

---
+++ src/_greedy/_1754/Solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def largestMerge(self, s1, s2):
+        if s1 >= s2 > '':
+            return s1[0] + self.largestMerge(s1[1:], s2)
+        if s2 >= s1 > '':
+            return s2[0] + self.largestMerge(s1, s2[1:])
+        return s1 + s2
