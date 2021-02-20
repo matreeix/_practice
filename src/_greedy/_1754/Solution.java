117774d75bf33ec6a9c2649e6619663316e28256
matreeix <2279142891@qq.com>
2021-02-21 00:48:22 +0800

    添加高效解法

--- src/_greedy/_1754/Solution.java
+++ src/_greedy/_1754/Solution.java
@@ -10,11 +10,64 @@ package _greedy._1754;
  */
 
 public class Solution {
+    //O(m*n)
     public String largestMerge(String word1, String word2) {
-        if (word1.length() == 0  || word2.length() == 0)
+        if (word1.length() == 0 || word2.length() == 0)
             return word1 + word2;
         if (word1.compareTo(word2) > 0)
             return word1.charAt(0) + largestMerge(word1.substring(1), word2);
         return word2.charAt(0) + largestMerge(word1, word2.substring(1));
+    }
+
+    public String largestMerge2(String word1, String word2) {
+        char[] a = word1.toCharArray();
+        char[] b = word2.toCharArray();
+        int n = a.length;
+        int m = b.length;
+        int i = 0;
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i < n && j < m) {
+            if (a[i] > b[j]) {
+                sb.append(a[i]);
+                i++;
+            } else if (b[j] > a[i]) {
+                sb.append(b[j]);
+                j++;
+            } else {
+                int x = i;
+                int y = j;
+                while (x < n && y < m && a[x] == b[y]) {
+                    x++;
+                    y++;
+                }
+                if (x == n && y == m) {
+                    sb.append(a[i++]);
+                    continue;
+                }
+                if (x == n) {
+                    sb.append(b[j++]);
+                    continue;
+                }
+                if (y == m) {
+                    sb.append(a[i++]);
+                    continue;
+                }
+                if (a[x] > b[y]) {
+                    sb.append(a[i++]);
+                } else {
+                    sb.append(b[j++]);
+                }
+            }
+        }
+        while (i < n) {
+            sb.append(a[i]);
+            i++;
+        }
+        while (j < m) {
+            sb.append(b[j]);
+            j++;
+        }
+        return sb.toString();
     }
 }
