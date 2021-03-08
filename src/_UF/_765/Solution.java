15a30f3f9264d75711f52287ccb584ed23161ec4
matreeix <2279142891@qq.com>
2021-03-08 17:48:08 +0800

    765. 情侣牵手

---
+++ src/_UF/_765/Solution.java
@@ -0,0 +1,60 @@
+package _UF._765;
+
+/**
+ * @Description: 765. 情侣牵手
+ * N 对情侣坐在连续排列的 2N 个座位上，想要牵到对方的手。 计算最少交换座位的次数，以便每对情侣可以并肩坐在一起。 一次交换可选择任意两人，让他们站起来交换座位。
+ * 人和座位用 0 到 2N-1 的整数表示，情侣们按顺序编号，第一对是 (0, 1)，第二对是 (2, 3)，以此类推，最后一对是 (2N-2, 2N-1)。
+ * 这些情侣的初始座位  row[i] 是由最初始坐在第 i 个座位上的人决定的。
+ * @Date: 2021/3/8
+ */
+
+public class Solution {
+
+    public int minSwapsCouples(int[] row) {
+        int len = row.length;
+        int N = len / 2;
+        UnionFind unionFind = new UnionFind(N);
+        for (int i = 0; i < len; i += 2) {
+            unionFind.union(row[i] / 2, row[i + 1] / 2);
+        }
+        return N - unionFind.getCount();
+    }
+
+    private class UnionFind {
+
+        private int[] parent;
+
+        private int count;
+
+        public int getCount() {
+            return count;
+        }
+
+        public UnionFind(int n) {
+            this.count = n;
+            this.parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            parent[rootX] = rootY;
+            count--;
+        }
+    }
+}
