5c68c843652b9a56a00891fcc9c10db62e19dcbf
matreeix <2279142891@qq.com>
2021-03-08 21:11:38 +0800

    1780. 判断一个数字是否可以表示成三的幂的和

--- src/_UF/_765/Solution.java
+++ src/_UF/_765/Solution.java
@@ -10,6 +10,35 @@ package _UF._765;
 
 public class Solution {
 
+    //O(N)
+    public int minSwapsCouples2(int[] row) {
+        int res = 0, N = row.length;
+
+        int[] ptn = new int[N];
+        int[] pos = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            ptn[i] = (i % 2 == 0 ? i + 1 : i - 1);
+            pos[row[i]] = i;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = ptn[pos[ptn[row[i]]]]; i != j; j = ptn[pos[ptn[row[i]]]]) {
+                swap(row, i, j);
+                swap(pos, row[i], row[j]);
+                res++;
+            }
+        }
+        return res;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    //并查集，NLogN
     public int minSwapsCouples(int[] row) {
         int len = row.length;
         int N = len / 2;
---
+++ src/_math/_1780/Solution.java
@@ -0,0 +1,29 @@
+package _math._1780;
+
+/**
+ * @Description: 1780. 判断一个数字是否可以表示成三的幂的和
+ * 给你一个整数 n ，如果你可以将 n 表示成若干个不同的三的幂之和，请你返回 true ，否则请返回 false 。
+ * 对于一个整数 y ，如果存在整数 x 满足 y == 3x ，我们称这个整数 y 是三的幂。
+ * @Date: 2021/3/8
+ */
+
+public class Solution {
+    public static boolean checkPowersOfThree(int n) {
+        while (n != 0) {
+            if (n % 3 == 0) {
+                n /= 3;
+            } else if (n % 3 == 1) {
+                n -= 1;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPowersOfThree(12));
+        System.out.println(checkPowersOfThree(91));
+        System.out.println(checkPowersOfThree(21));
+    }
+}
