17d20ba530f92284be3643c3f31e7a7f2f017d55
matreeix <2279142891@qq.com>
2021-03-20 20:17:20 +0800

    895. 最大频率栈

---
+++ src/_stack/_895/FreqStack.java
@@ -0,0 +1,59 @@
+package _stack._895;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+/**
+ * @Description: 895. 最大频率栈
+ * 实现 FreqStack，模拟类似栈的数据结构的操作的一个类。
+ * FreqStack 有两个函数：
+ * 1.push(int x)，将整数 x 推入栈中。
+ * 2.pop()，它移除并返回栈中出现最频繁的元素。
+ * 如果最频繁的元素不只一个，则移除并返回最接近栈顶的元素。
+ * @Date: 2021/3/20
+ */
+
+public class FreqStack {
+    Map<Integer, Integer> freq;
+    Map<Integer, Stack<Integer>> group;
+    int maxfreq;
+
+    public FreqStack() {
+        freq = new HashMap();
+        group = new HashMap();
+        maxfreq = 0;
+    }
+
+    public void push(int x) {
+        int f = freq.getOrDefault(x, 0) + 1;
+        freq.put(x, f);
+        if (f > maxfreq)
+            maxfreq = f;
+
+        group.computeIfAbsent(f, z-> new Stack()).push(x);
+    }
+
+    public int pop() {
+        int x = group.get(maxfreq).pop();
+        freq.put(x, freq.get(x) - 1);
+        if (group.get(maxfreq).size() == 0)
+            maxfreq--;
+        return x;
+    }
+
+    public static void main(String[] args) {
+        FreqStack obj = new FreqStack();
+        obj.push(5);
+        obj.push(7);
+        obj.push(5);
+        obj.push(7);
+        obj.push(4);
+        obj.push(5);
+        System.out.println(obj.pop());
+        System.out.println(obj.pop());
+        System.out.println(obj.pop());
+        System.out.println(obj.pop());
+    }
+}
