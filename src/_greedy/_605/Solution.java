bc1fc543532ea7dfd8794ffa813514302420f038
matreeix <2279142891@qq.com>
2021-03-27 14:39:27 +0800

    509、530、575、594、598、599、605

---
+++ src/_greedy/_605/Solution.java
@@ -0,0 +1,46 @@
+package _greedy._605;
+
+/**
+ * @Description: 605. 种花问题
+ * 假设有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+ * 给你一个整数数组  flowerbed 表示花坛，由若干 0 和 1 组成，其中 0 表示没种植花，1 表示种植了花。另有一个数 n ，能否在不打破种植规则的情况下种入 n 朵花？能则返回 true ，不能则返回 false。
+ * @Date: 2021/3/27
+ */
+
+public class Solution {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) return true;
+        int len = flowerbed.length;
+        int cnt = 1;//统计连续0的个数，初始左边加个0
+        int count = 0;
+        for (int f : flowerbed) {
+            if (f == 0) {
+                cnt++;
+            } else {
+                count += cnt >= 3 ? (cnt - 1) / 2 : 0;
+                cnt = 0;
+            }
+        }
+        cnt++;//最右边也加一个0
+        count += cnt >= 3 ? (cnt - 1) / 2 : 0;
+        return count >= n;
+    }
+
+    public boolean canPlaceFlowers2(int[] flowerbed, int n) {
+        // 每次跳两格,因为如果遇到1,那么下一格子一定是0
+        for (int i = 0; i < flowerbed.length; i += 2) {
+            // 如果当前为空地
+            if (flowerbed[i] == 0) {
+                // 如果是最后一格或者下一格为空
+                if (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) {
+                    n--;
+                } else {//flowerbed[i + 1] == 1
+                    i++;//跳到下个1
+                }
+            }
+        }
+        return n <= 0;
+    }
+
+}
---
+++ src/_hashtable/_599/Solution.java
@@ -0,0 +1,61 @@
+package _hashtable._599;
+
+import java.util.*;
+
+/**
+ * @Description: 599. 两个列表的最小索引总和
+ * 假设Andy和Doris想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。
+ * 你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设总是存在一个答案。
+ * <p>
+ * 提示:
+ * <p>
+ * 两个列表的长度范围都在 [1, 1000]内。
+ * 两个列表中的字符串的长度将在[1，30]的范围内。
+ * 下标从0开始，到列表的长度减1。
+ * 两个列表都没有重复的元素。
+ * @Date: 2021/3/27
+ */
+
+public class Solution {
+
+    //哈希表1，7ms
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < list1.length; i++)
+            map.put(list1[i], i);
+        List<String> res = new ArrayList<>();
+        int min_sum = Integer.MAX_VALUE, sum;
+        for (int j = 0; j < list2.length && j <= min_sum; j++) {
+            if (map.containsKey(list2[j])) {
+                sum = j + map.get(list2[j]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[j]);
+                    min_sum = sum;
+                } else if (sum == min_sum)
+                    res.add(list2[j]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+
+    //哈希表2,114ms
+    public String[] findRestaurant2(String[] list1, String[] list2) {
+        HashMap<Integer, List<String>> map = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j])) {
+                    if (!map.containsKey(i + j))
+                        map.put(i + j, new ArrayList<>());
+                    map.get(i + j).add(list1[i]);
+                }
+            }
+        }
+        int min_index_sum = Integer.MAX_VALUE;
+        for (int key : map.keySet())
+            min_index_sum = Math.min(min_index_sum, key);
+        String[] res = new String[map.get(min_index_sum).size()];
+        return map.get(min_index_sum).toArray(res);
+    }
+
+}
