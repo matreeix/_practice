549b6a5cb017ac822188b57c46cfe88fb4ea1e3f
matreeix <2279142891@qq.com>
2021-04-01 23:01:56 +0800

    13、14、155、172

---
+++ src/_stack/_155/MinStack2.java
@@ -0,0 +1,51 @@
+package _stack._155;
+
+/**
+ * @Description: 不使用自带的栈数据结构
+ * @Date: 2021/4/1
+ */
+
+public class MinStack2 {
+    class Node {
+        int value;
+        int min;
+        Node next;
+
+        Node(int x, int min) {
+            this.value = x;
+            this.min = min;
+            next = null;
+        }
+    }
+
+    Node head;
+
+    //每次加入的节点放到头部
+    public void push(int x) {
+        if (null == head) {
+            head = new Node(x, x);
+        } else {
+            //当前值和之前头结点的最小值较小的做为当前的 min
+            Node n = new Node(x, Math.min(x, head.min));
+            n.next = head;
+            head = n;
+        }
+    }
+
+    public void pop() {
+        if (head != null)
+            head = head.next;
+    }
+
+    public int top() {
+        if (head != null)
+            return head.value;
+        return -1;
+    }
+
+    public int getMin() {
+        if (null != head)
+            return head.min;
+        return -1;
+    }
+}
---
+++ src/_stack/_155/MinStack3.java
@@ -0,0 +1,41 @@
+package _stack._155;
+
+import java.util.Stack;
+
+/**
+ * @Description: 使用一个栈
+ * @Date: 2021/4/1
+ */
+
+public class MinStack3 {
+
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+
+    public void push(int x) {//这种操作会压入很多无关的元素，并不会节省空间，只是一种炫技。。。。
+        //当前值更小
+        if (x <= min) {
+            //将之前的最小值保存
+            stack.push(min);
+            //更新最小值
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        //如果弹出的值是最小值，那么将下一个元素更新为最小值
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+}
