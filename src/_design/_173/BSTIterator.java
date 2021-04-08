083cb27a5a1122fe31663e71c077166282ae18bf
matreeix <2279142891@qq.com>
2021-04-08 14:06:37 +0800

    151、153、173

---
+++ src/_design/_173/BSTIterator.java
@@ -0,0 +1,65 @@
+package _design._173;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Description: 173. 二叉搜索树迭代器
+ * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+ * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+ * int next()将指针向右移动，然后返回指针处的数字。
+ * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点的数目在范围 [1, 105] 内
+ * 0 <= Node.val <= 106
+ * 最多调用 105 次 hasNext 和 next 操作
+ * @Date: 2021/4/8
+ */
+
+public class BSTIterator {
+    private Queue<Integer> q;
+
+    public BSTIterator(TreeNode root) {
+        this.q = new LinkedList<>();
+//        q.add(-1);
+        dfs(root);
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.left);
+        q.add(root.val);
+        dfs(root.right);
+    }
+
+    public int next() {
+        return q.poll();
+    }
+
+    public boolean hasNext() {
+        return q.size() > 0;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
