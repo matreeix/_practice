d632f2cb2ecc188246defbe6244112e52e71e017
matreeix <2279142891@qq.com>
2021-04-10 20:13:41 +0800

    199、201、208

---
+++ src/_bit_calc/_201/Solution.java
@@ -0,0 +1,42 @@
+
+package _bit_calc._201;
+
+/**
+ * @Description: 201. 数字范围按位与
+ * 给你两个整数 left 和 right ，表示区间 [left, right] ，返回此区间内所有数字 按位与 的结果（包含 left 、right 端点）。
+ * 提示：
+ * 0 <= left <= right <= 2^31 - 1
+ * @Date: 2021/4/10
+ */
+
+public class Solution {
+    /**
+     * 1. n & (n + 1) = n,n为偶数
+     * 2. 2^n & (2^n+1)&....&(2^(n+1)-1) = 2^n
+     * 3. 2^n & 2^(n+1) = 0
+     */
+    public int rangeBitwiseAnd(int m, int n) {
+        int shift = 0;
+        // 找到公共前缀
+        while (m < n) {
+            m >>= 1;
+            n >>= 1;
+            ++shift;
+        }
+        return m << shift;
+    }
+
+    //Brian Kernighan 算法
+    public int rangeBitwiseAnd2(int m, int n) {
+        while (m < n) {
+            // 抹去最右边的 1
+            n = n & (n - 1);
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(2 & 2);
+    }
+
+}
---
+++ src/_tree/n_ary_tree/_208/Trie.java
@@ -0,0 +1,71 @@
+package _tree.n_ary_tree._208;
+
+/**
+ * @Description: 208. 实现 Trie (前缀树)
+ * Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+ * 请你实现 Trie 类：
+ * 1.Trie() 初始化前缀树对象。
+ * 2.void insert(String word) 向前缀树中插入字符串 word 。
+ * 3.boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+ * 4.boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+ * 提示：
+ * 1 <= word.length, prefix.length <= 2000
+ * word 和 prefix 仅由小写英文字母组成
+ * insert、search 和 startsWith 调用次数 总计 不超过 3 * 104 次
+ * @Date: 2021/4/10
+ */
+
+class TrieNode {
+    public char val;
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+
+    public TrieNode() {
+    }
+
+    TrieNode(char c) {
+        TrieNode node = new TrieNode();
+        node.val = c;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+        root.val = ' ';
+    }
+
+    public void insert(String word) {
+        TrieNode ws = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (ws.children[c - 'a'] == null) {
+                ws.children[c - 'a'] = new TrieNode(c);
+            }
+            ws = ws.children[c - 'a'];
+        }
+        ws.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode ws = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return ws.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode ws = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return true;
+    }
+}
