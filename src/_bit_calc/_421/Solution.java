a023237fb8d3983394fc23b9ab17bb135520a64c
matreeix <2279142891@qq.com>
2021-05-27 22:26:02 +0800

    421. 数组中两个数的最大异或值

---
+++ src/_bit_calc/_421/Solution.java
@@ -0,0 +1,136 @@
+package _bit_calc._421;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description: 421. 数组中两个数的最大异或值
+ * 给你一个整数数组 nums ，返回 nums[i] XOR nums[j] 的最大运算结果，其中 0 ≤ i ≤ j < n 。
+ * 进阶：你可以在 O(n) 的时间解决这个问题吗？
+ * @Created by: matreeix
+ * @Date: 2021/5/26
+ */
+public class Solution {
+    // 字典树的根节点
+    Trie root = new Trie();
+    // 最高位的二进制位编号为 30
+    static final int HIGH_BIT = 30;
+
+    public int findMaximumXOR(int[] nums) {
+        int n = nums.length;
+        int x = 0;
+        for (int i = 1; i < n; ++i) {
+            // 将 nums[i-1] 放入字典树，此时 nums[0 .. i-1] 都在字典树中
+            add(nums[i - 1]);
+            // 将 nums[i] 看作 ai，找出最大的 x 更新答案
+            x = Math.max(x, check(nums[i]));
+        }
+        return x;
+    }
+
+    public void add(int num) {
+        Trie cur = root;
+        for (int k = HIGH_BIT; k >= 0; --k) {
+            int bit = (num >> k) & 1;
+            if (bit == 0) {
+                if (cur.left == null)
+                    cur.left = new Trie();
+                cur = cur.left;
+            } else {
+                if (cur.right == null)
+                    cur.right = new Trie();
+                cur = cur.right;
+            }
+        }
+    }
+
+    public int check(int num) {
+        Trie cur = root;
+        int x = 0;
+        for (int k = HIGH_BIT; k >= 0; --k) {
+            int bit = (num >> k) & 1;
+            if (bit == 0) {
+                // a_i 的第 k 个二进制位为 0，应当往表示 1 的子节点 right 走
+                if (cur.right != null) {
+                    cur = cur.right;
+                    x = x * 2 + 1;
+                } else {
+                    cur = cur.left;
+                    x = x * 2;
+                }
+            } else {
+                // a_i 的第 k 个二进制位为 1，应当往表示 0 的子节点 left 走
+                if (cur.left != null) {
+                    cur = cur.left;
+                    x = x * 2 + 1;
+                } else {
+                    cur = cur.right;
+                    x = x * 2;
+                }
+            }
+        }
+        return x;
+    }
+
+
+    public int findMaximumXOR2(int[] nums) {
+        int x = 0;
+        for (int k = HIGH_BIT; k >= 0; --k) {
+            Set<Integer> seen = new HashSet<Integer>();
+            // 将所有的 pre^k(a_j) 放入哈希表中
+            for (int num : nums) {
+                // 如果只想保留从最高位开始到第 k 个二进制位为止的部分,只需将其右移 k 位
+                seen.add(num >> k);
+            }
+
+            // 目前 x 包含从最高位开始到第 k+1 个二进制位为止的部分
+            // 我们将 x 的第 k 个二进制位置为 1，即为 x = x*2+1
+            int xNext = x * 2 + 1;
+            boolean found = false;
+
+            // 枚举 i
+            for (int num : nums) {
+                if (seen.contains(xNext ^ (num >> k))) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                x = xNext;
+            } else {
+                // 如果没有找到满足等式的 a_i 和 a_j，那么 x 的第 k 个二进制位只能为 0
+                // 即为 x = x*2
+                x = xNext - 1;
+            }
+        }
+        return x;
+    }
+
+    public int findMaximumXOR3(int[] nums) {
+        int max = 0, mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask = mask | (1 << i);
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num & mask);
+            }
+            int tmp = max | (1 << i);
+            for (int prefix : set) {
+                if (set.contains(tmp ^ prefix)) {
+                    max = tmp;
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
+
+class Trie {
+    // 左子树指向表示 0 的子节点
+    Trie left = null;
+    // 右子树指向表示 1 的子节点
+    Trie right = null;
+}
+
