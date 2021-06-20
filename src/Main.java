d5175239ec633a1414df2b638c0d7e39565354bd
matreeix <2279142891@qq.com>
2021-06-20 23:02:56 +0800

    项目修改

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 /**
- * @Description: Leetcode刷题总结
+ * @Description: 算法刷题总结
  * @Author: matreeix
  * @Date: 2020/8/26
  */
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,LeetCode!");
+        System.out.println("Have you done your exercises today ?");
     }
 }
 
