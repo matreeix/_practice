e56b8e7a1c35d914b45c861b63ce2064bba875c9
matreeix <2279142891@qq.com>
2021-06-24 00:38:24 +0800

    740. 删除并获得点数

---
+++ src/_leetcode/_DP/_740/Solution.java
@@ -0,0 +1,41 @@
+package _leetcode._DP._740;
+
+
+/**
+ * @Description: 740. 删除并获得点数
+ * 给你一个整数数组 nums ，你可以对它进行一些操作。
+ * 每次操作中，选择任意一个 nums[i] ，删除它并获得 nums[i] 的点数。之后，你必须删除 所有 等于 nums[i] - 1 和 nums[i] + 1 的元素。
+ * 开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+ * @Created by: matreeix
+ * @Date: 2021/6/23
+ */
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+    public int deleteAndEarn(int[] nums) {
+        int maxVal = 0;
+        for (int val : nums) {
+            maxVal = Math.max(maxVal, val);
+        }
+        int[] sum = new int[maxVal + 1];
+        for (int val : nums) {
+            sum[val] += val;
+        }
+        return rob(sum);
+    }
+}
