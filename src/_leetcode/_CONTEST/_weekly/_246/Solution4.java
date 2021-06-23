2e0bd92bc2918fcb0225f9740f03f4dddda23a11
matreeix <2279142891@qq.com>
2021-06-23 22:51:28 +0800

    添加注释

--- src/_leetcode/_CONTEST/_weekly/_246/Solution4.java
+++ src/_leetcode/_CONTEST/_weekly/_246/Solution4.java
@@ -12,9 +12,9 @@ package _leetcode._CONTEST._weekly._246;
  * 如果 x >= 0 ，那么值为 x 。
  * 如果 x < 0 ，那么值为 -x 。
  * 提示：
- * 2 <= nums.length <= 105
+ * 2 <= nums.length <= 10^5
  * 1 <= nums[i] <= 100
- * 1 <= queries.length <= 2 * 104
+ * 1 <= queries.length <= 2 * 10^4
  * 0 <= li < ri < nums.length
  * @Created by: matreeix
  * @Date: 2021/6/21
@@ -22,8 +22,12 @@ package _leetcode._CONTEST._weekly._246;
 public class Solution4 {
     public int[] minDifference(int[] nums, int[][] queries) {
         int len = nums.length;
-        int[][] count = new int[len + 1][101];
+        int[][] count = new int[len + 1][101];//count[i][c] 表示数组 nums 的前缀 idx[0..i−1] 中包含元素 c 的个数
         for (int i = 1; i <= len; i++) {
+            /**
+             * for(int j = 0; j < 100; ++j)
+             *   count[i][j] = count[i-1][j];
+             * */
             System.arraycopy(count[i - 1], 0, count[i], 0, 101);
             count[i][nums[i - 1]]++;
         }
@@ -34,10 +38,12 @@ public class Solution4 {
             int last = -1;
             int min = Integer.MAX_VALUE;
             for (int j = 1; j <= 100; j++) {
-                if (count[end + 1][j] - count[start][j] > 0) {
-                    if (last >= 0) {
-                        min = Math.min(min, j - last);
-                    }
+                /**
+                 * 对于任意的数组 a，如果 a 已经有序，那么我们只需要对 a 进行一次遍历，得到 a 中相邻两元素的差值中的最小值（不能为 0），
+                 * 即为「差绝对值的最小值」。如果 a 中所有元素均相等，那么「差绝对值的最小值」为 −1。
+                 * */
+                if (count[end + 1][j] - count[start][j] > 0) {//num[start,end]包含元素j
+                    if (last >= 0) min = Math.min(min, j - last);
                     last = j;
                 }
             }
