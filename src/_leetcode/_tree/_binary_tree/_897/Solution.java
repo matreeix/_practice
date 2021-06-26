6a4a5be0ab560118c44f24f22eff0d6a8c24cb05
matreeix <2279142891@qq.com>
2021-06-26 22:26:48 +0800

    897. 递增顺序搜索树

---
+++ src/_leetcode/_tree/_binary_tree/_897/Solution.java
@@ -0,0 +1,54 @@
+package _leetcode._tree._binary_tree._897;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: 897. 递增顺序搜索树
+ * 给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
+ * @Created by: matreeix
+ * @Date: 2021/6/26
+ */
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+
+        TreeNode dummyNode = new TreeNode(-1);
+        TreeNode currNode = dummyNode;
+        for (int value : res) {
+            currNode.right = new TreeNode(value);
+            currNode = currNode.right;
+        }
+        return dummyNode.right;
+    }
+
+    public void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }
+
+}
