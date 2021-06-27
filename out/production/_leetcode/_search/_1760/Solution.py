f001128a9e8b329f08da6f2d0bbaf405b7b3b5ff
matreeix <2279142891@qq.com>
2021-06-27 21:05:15 +0800

    删除了out

--- out/production/_leetcode/_search/_1760/Solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        lo, hi = 1, 1_000_000_000
-        while lo < hi:
-            mid = lo + hi >> 1
-            if sum((x-1)//mid for x in nums) <= maxOperations: hi = mid
-            else: lo = mid + 1
-        return lo
