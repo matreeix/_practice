173c1572820ec92d0bc3317c586c5f6cf985454c
matreeix <2279142891@qq.com>
2021-07-08 23:42:43 +0800

    524. 通过删除字母匹配到字典里最长单词

---
+++ src/_leetcode/_string/_524/Solution.java
@@ -0,0 +1,32 @@
+package _leetcode._string._524;
+
+import java.util.List;
+
+/**
+ * @Description: 524. 通过删除字母匹配到字典里最长单词
+ * 给你一个字符串 s 和一个字符串数组 dictionary 作为字典，找出并返回字典中最长的字符串，该字符串可以通过删除 s 中的某些字符得到。
+ * 如果答案不止一个，返回长度最长且字典序最小的字符串。如果答案不存在，则返回空字符串。
+ * @Date: 2021/7/8
+ */
+
+public class Solution {
+    public boolean isSubsequence(String x, String y) {
+        int j = 0;
+        for (int i = 0; i < y.length() && j < x.length(); i++)
+            if (x.charAt(j) == y.charAt(i))
+                j++;
+        return j == x.length();
+    }
+
+    public String findLongestWord(String s, List<String> d) {
+        String max_str = "";
+        for (String str : d) {
+            if (isSubsequence(str, s)) {
+                if (str.length() > max_str.length() || (str.length() == max_str.length() && str.compareTo(max_str) < 0))
+                    max_str = str;
+            }
+        }
+        return max_str;
+    }
+
+}
