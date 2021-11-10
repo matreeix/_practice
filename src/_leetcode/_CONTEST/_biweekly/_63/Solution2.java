bd999a66ac52ae0da1e9aeb8e4b2387e106121f4
matreeix <2279142891@qq.com>
2021-11-10 23:49:51 +0800

    5886. 优化

--- src/_leetcode/_CONTEST/_biweekly/_63/Solution2.java
+++ src/_leetcode/_CONTEST/_biweekly/_63/Solution2.java
@@ -47,6 +47,19 @@ public class Solution2 {
         }
     }
 
+    public boolean winnerOfGame2(String s) {
+        int a = 0, b = 0;
+        for (int i = 1; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i - 1) && s.charAt(i) == s.charAt(i + 1)) {
+                if (s.charAt(i) == 'A')
+                    a++;
+                else
+                    b++;
+            }
+        }
+        return a > b;
+    }
+
     public static void main(String[] args) {
         String s1 = "AAAABBBB";
         String s2 = "BBBBAAAA";
