7e2f79b45900c523a3a31da33a842589e1485719
matreeix <2279142891@qq.com>
2022-02-15 23:29:17 +0800

    2169. 得到 0 的操作数

---
+++ src/_leetcode/_CONTEST/_weekly/_280/Solution1.java
@@ -0,0 +1,18 @@
+package _leetcode._CONTEST._weekly._280;
+
+/**
+ * @Description: 2169. 得到 0 的操作数
+ * 给你两个 非负 整数 num1 和 num2 。
+ * 每一步 操作 中，如果 num1 >= num2 ，你必须用 num1 减 num2 ；否则，你必须用 num2 减 num1 。
+ * 例如，num1 = 5 且 num2 = 4 ，应该用 num1 减 num2 ，因此，得到 num1 = 1 和 num2 = 4 。然而，如果 num1 = 4且 num2 = 5 ，一步操作后，得到 num1 = 4 和 num2 = 1 。
+ * 返回使 num1 = 0 或 num2 = 0 的 操作数 。
+ * @Date: 2022/2/15
+ */
+
+public class Solution1 {
+    public int countOperations(int num1, int num2) {
+        if (num1 * num2 == 0) return 0;
+        if (num2 > num1) return countOperations(num2, num1);
+        return countOperations(num2, num1 % num2) + num1 / num2;
+    }
+}
